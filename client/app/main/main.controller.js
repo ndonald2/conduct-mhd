020e034fe3091fd203a6f4a8c9bb48dba87eb870
Nick Donaldson <ndonald2@gmail.com>
2014-11-08 15:47:51 -0500

    Cleanup initial yeoman example

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,26 +2,13 @@
 
 angular.module('conductorMhdApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
-    $scope.awesomeThings = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
-    });
+    //$http.get('/api/things').success(function(awesomeThings) {
+    //  $scope.awesomeThings = awesomeThings;
+    //  socket.syncUpdates('thing', $scope.awesomeThings);
+    //});
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
-
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
-    });
+    //$scope.$on('$destroy', function () {
+    //  socket.unsyncUpdates('thing');
+    //});
   });
