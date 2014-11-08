f6937001019bad17ff54697e64de979a7cb7f602
Nick Donaldson <ndonald2@gmail.com>
2014-11-08 16:01:55 -0500

    Basic ntp ping is working

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,6 +3,10 @@
 angular.module('conductorMhdApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
 
+    socket.socket.emit('ntp', {timeStamp: new Date().getTime() });
+    socket.socket.on('ntp', function(data) {
+      console.log('ntp :', data.serverReceivedTime);
+    });
     //$http.get('/api/things').success(function(awesomeThings) {
     //  $scope.awesomeThings = awesomeThings;
     //  socket.syncUpdates('thing', $scope.awesomeThings);
