cb8d03c37edbfd589004c51470de67109190a281
Nick Donaldson <ndonald2@gmail.com>
2014-11-08 15:21:01 -0500

    Comment about not needing mongo

--- server/app.js
+++ server/app.js
@@ -8,10 +8,11 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('express');
-//var mongoose = require('mongoose');
 var config = require('./config/environment');
 
 // Connect to database
+// NOT NEEDED RIGHT NOW
+//var mongoose = require('mongoose');
 //mongoose.connect(config.mongo.uri, config.mongo.options);
 
 // Populate DB with sample data
