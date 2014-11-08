5952be41fc79fcc763908f92aeccf498aee9d574
Nick Donaldson <ndonald2@gmail.com>
2014-11-08 15:13:42 -0500

    Initial commit after bootstrapping with yeoman

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('conductorMhdApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
