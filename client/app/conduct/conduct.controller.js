5e8b30e20213ec1a67482391369bd49532cd7d78
Nick Donaldson <ndonald2@gmail.com>
2014-11-09 14:05:34 -0500

    watching the controls

--- client/app/conduct/conduct.controller.js
+++ client/app/conduct/conduct.controller.js
@@ -1,6 +1,20 @@
 'use strict';
 
 angular.module('conductorMhdApp')
-  .controller('ConductCtrl', function ($scope) {
-    $scope.fake = 'Fake';
+  .controller('ConductCtrl', function ($scope, control) {
+
+    $scope.controlsReady = false;
+
+    var controls = {
+      'melody:volume': { id: 'melody:volume', name: 'Melody Volume', min: -100, max: 0, value: -6 },
+      'bass:volume': { id: 'bass:volume', name: 'Bass Volume', min: -100, max: 0, value: -6 },
+      'sprinkle:volume': { id: 'sprinkle:volume', name: 'Sprinkle Volume', min: -100, max: 0, value: -6 }
+    };
+
+    $scope.controls = _.values(controls); 
+    $scope.changed = function(id) {
+      var diff = {};
+      diff[id] = controls[id].value;
+      control.update(diff); 
+    };
   });
