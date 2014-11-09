2c1682cc56c1a0538417af248293bd8a3fd8a3ab
Nick Donaldson <ndonald2@gmail.com>
2014-11-09 00:24:57 -0500

    Move latency measurement ping to perform view

--- client/app/perform/perform.controller.js
+++ client/app/perform/perform.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('conductorMhdApp')
-  .controller('PerformCtrl', function ($scope, $location, $routeParams, constants) {
+  .controller('PerformCtrl', function ($scope, $location, $routeParams, ntp, constants) {
     var side = $routeParams.side ? $routeParams.side : 'A';
   
     // If it's not a valid route param, just bail out to the main page
@@ -10,4 +10,24 @@ angular.module('conductorMhdApp')
     }
 
     $scope.side = side;
+    var evaluateBestLatency = function() {
+        var latency = ntp.getBestRoundtripLatency();
+        $scope.pingLatency = latency;      
+        return (latency < constants.maxLatency);
+    };
+
+    $scope.waitingForPing = false;
+    
+    ntp.startMeasurements();
+    $scope.$on('ntp:update', function() {
+      var goodEnough = evaluateBestLatency(); 
+      $scope.waitingForPing = !goodEnough;
+      if (goodEnough) {
+        ntp.stopMeasurements();
+      }
+    });
+
+    $scope.$on('$destroy', function() {
+      ntp.stopMeasurements();
+    });
   });
